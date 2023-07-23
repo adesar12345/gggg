332b8c55d71ce654ad7055c47e0f5398b6a54dec
adesar12345 <102373741+adesar12345@users.noreply.github.com>
2023-07-23 03:23:44 +0300

    Add files via upload

---
+++ access/values.js
@@ -0,0 +1,24 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = true; // Права
+var isCovidCertificateEnabled = true; // COVID-сертификат
+
+// Общее
+var birthdate = "20.12.2004"; // Дата рождения
+var fullname = "Кучер Назар Олександрович" // ФИО
+var name = "Назар" // Имя
+
+// Паспорт
+var passport_id = "008695120" // Номер паспорта
+
+// КПП
+var kpp_id = "3834007097" // ИНН
+
+// Права
+var rights_valid_until = "30.7.2025" // Права "Дійсні до"
+var rights_categories = "A, B" // Права "Категорії"
+var rights_tsc = "ТСЦ 8631" // Права "Видав
+var rights_id = "KBE968639" // Права "Серія та номер""
+
+// COVID-сертификат
+var covid_valid_until = "27.12.2022" // Сертификат "Дійсний до"
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
